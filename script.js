4ce00bf89b86608229fccbf60669d667d2584e2c
DESKTOP-VF9OCE7\allex <allexortiz@outlook.com>
2023-09-20 21:41:42 -0500

    made variables for ever case of characters and added them all together

--- script.js
+++ script.js
@@ -3,18 +3,33 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword() {
+    alert("A strong password must should a combination of uppercase letters, lowercase letters, numbers, and special characters and be at least 8 characters long but no more than 128.");
     var length = parseInt(prompt("Enter password length from 8 to 128 characters"));
-    
+
     if (length < 8 || length > 128) {
-      alert("Invalid length");
-      return;
+        alert("Invalid length");
+        return;
     }
 
-    var lowerChars = ""
+    var lowerChars = confirm("Would you like to include lowercase characters?");
+    var upperChars = confirm("Would you like to include uppercase characters?");
+    var numericChars = confirm("Would you like to include numeric characters?");
+    var specialChars = confirm("Would you like to include special characters?");
 
+    if (!lowerChars || !upperChars || !numericChars || !specialChars) {
+        alert("Please click okay on all criteria.");
+        return;
+        
+    }
 
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numeric = "0123456789";
+    var special = "!@#$%^&*()_-+=<>?";
 
+    var allChars = lowercase + uppercase + numeric + special
 
+for (var i = 0; i 
 
 
 
@@ -24,10 +39,10 @@ function generatePassword() {
 }
 
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 }
 
 // Add event listener to generate button
