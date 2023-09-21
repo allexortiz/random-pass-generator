d66bf6ed85b81927daa1f8304b4cf4aa91ff629f
DESKTOP-VF9OCE7\allex <allexortiz@outlook.com>
2023-09-20 20:48:19 -0500

    added code to make Invalid length pop up when entering a number less than 7 or greater than 128

--- script.js
+++ script.js
@@ -3,7 +3,24 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword() {
-var length = prompt("Enter password length from 8 to 128 characters");
+    var length = parseInt(prompt("Enter password length from 8 to 128 characters"));
+    
+    if (length < 8 || length > 128) {
+      alert("Invalid length");
+      return;
+    }
+
+    var lowerChars = ""
+
+
+
+
+
+
+
+
+
+
 }
 
 function writePassword() {
@@ -15,3 +32,36 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (length < 8) {
+//     alert("Password must be greater than 8 characters!")
+//   } else if (length > 128) {
+//     alert("Password must be less than 129!")
+//     return
+//   }
